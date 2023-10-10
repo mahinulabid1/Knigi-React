322ead4627bca10ecbaf93db8f25c3eb747dffb7
Mahinul <mahinulmostafa@gmail.com>
2023-10-10 23:33:39 +0600

    code optimized

--- src/component/childComponent/HamNavigation.jsx
+++ src/component/childComponent/HamNavigation.jsx
@@ -1,37 +1,41 @@
 import { useState, useEffect } from "react";
-// import {hamSwitch, setHamSwitch} from "./nav"
 
-const HamNavigation = ({ hamClick }) => {
-    const [display, setDisplay] = useState("d-none");
-    const [animation, setAnimation] = useState("slideUp");
 
-    useEffect(() => {
-        if (hamClick === 1) {
-            setAnimation("slideDown");
-            setDisplay("");
+const HamNavigation = ( { hamClick } ) => {
+    const [ display, setDisplay ] = useState ( "d-none" );
+    const [ animation, setAnimation] = useState ( "slideUp" );
+
+    useEffect( () => {
+        if ( hamClick === 1 ) {
+            setAnimation( "slideDown" );
+            setDisplay( "" );
         }
-    }, [hamClick]);
+    }, [ hamClick ]);
+
+
+
+    const NavLinksInfo = [
+        {link : "/allStories" , text : "STORIES"} ,
+        {link : "/films" , text : "FILMS"} ,
+        {link : "/shop" , text : "SHOP"} ,
+        {link : "/blog" , text : "BLOG"} ,
+        {link : "/about" , text : "ABOUT"} 
+    ]
 
     return (
         <>
             {/* -- HAM NAVIGATION --*/}
-            <div className={display + " nav-in-ham " + animation}>
-                <div className="nav-in-ham-content-container">
-                    <a href="./allStories.php" className="nav-in-ham-content">
-                        STORIES
-                    </a>
-                    <a href="./films.php" className="nav-in-ham-content">
-                        FILMS
-                    </a>
-                    <a href="./shopall.php" className="nav-in-ham-content">
-                        SHOP
-                    </a>
-                    <a href="./allBlogs.php" className="nav-in-ham-content">
-                        BLOG
-                    </a>
-                    <a href="./contactUs.php" className="nav-in-ham-content">
-                        ABOUT
-                    </a>
+            <div className = { display + " nav-in-ham " + animation } >
+                < div className = "nav-in-ham-content-container" >
+                
+                    {
+                        NavLinksInfo.map( (cur,index) => {
+                            return(
+                                <a href={cur.link} className="nav-in-ham-content" key={index}>{cur.text}</a>
+                            )
+                        })
+                    }
+
                     <a href="./contactUs.php" className="request-interview-btn">
                         REQUEST INTERVIEW
                     </a>
@@ -46,16 +50,15 @@ const HamNavigation = ({ hamClick }) => {
                             }, 370 )
 
                             }
-                        } 
-                        
-                        className="ham-close-btn" id="close-nav-in-ham">
+                        }      
+                    className = "ham-close-btn" id = "close-nav-in-ham">
 
                         
-                        <img src="images/icons/close.png" alt="" height="20" width="20" />
+                        <img src = "images/icons/close.png" alt = "" height = "20" width = "20" />
+
                     </div>
                 </div>
             </div>
-            {/* -- HAM NAVIGATION END -*/}
         </>
     );
 };
