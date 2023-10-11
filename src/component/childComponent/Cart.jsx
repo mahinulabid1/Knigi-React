04619a7a7f422213039293eeac262651b642a386
Mahinul <mahinulmostafa@gmail.com>
2023-10-11 12:26:48 +0600

    added cart functionalities

--- src/component/childComponent/Cart.jsx
+++ src/component/childComponent/Cart.jsx
@@ -1,11 +1,22 @@
-const Cart = () => {
-    return (
+import {useState, useEffect} from "react";
+
+const Cart = ( { cartClick } ) => {
+    const [ displayCart, setDisplayCart ] = useState("d-none");
+
+    useEffect(() => {
+        if ( cartClick === 1 ) {
+            setDisplayCart ( "" );
+        }
+    }, [ cartClick ]);
+
+    return (//"cart-holder d-none"
         <>
-            {/* <!-- +++++++ CART HOLDER  ++++++++++++++++++--> */}
-            <div className="cart-holder d-none"> {/*<!-- d-none className will be here -->*/}
+            {/*  CART HOLDER  */}
+            {/*<!-- d-none className will be here -->*/}
+            <div className= {"cart-holder " + displayCart } > 
                 <div className="flex flex-s-b search-box">
                     <div>Your Cart <span>$35 USD</span></div>
-                    <div id="cart-close"><img src="images/icons/close.png" alt="" height="20" width="20" /></div>
+                    <div id="cart-close" onClick={()=>{setDisplayCart("d-none")}}><img src="images/icons/close.png" alt="" height="20" width="20" /></div>
                 </div>
 
                 <div className="items-in-cart-container">
