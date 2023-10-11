1cac23464c06d156b3582713ce6198f1c538e320
Mahinul <mahinulmostafa@gmail.com>
2023-10-11 10:35:12 +0600

    optimized code

--- src/component/childComponent/ShopNavigation.jsx
+++ src/component/childComponent/ShopNavigation.jsx
@@ -1,104 +1,125 @@
-import { useState , useEffect } from "react";
+import { useState, useEffect } from "react";
 
 
-const ShopNavigation = ( { shopClick } ) => {
+const ShopNavigation = ({ shopClick }) => {
 
-    const [ displayShop, setDisplayShop ] = useState ("d-none");
-    const [ animation, setAnimation ] = useState ('slideUp');  
-    
+    const [displayShop, setDisplayShop] = useState("d-none");
+    const [animation, setAnimation] = useState('slideUp');
+
+
     // auto close prevent on hover
-    const [ time, setTime ] = useState (0);
+    const [time, setTime] = useState(0);
 
 
-    useEffect( () => {
-        if ( shopClick === 1 ) {
-            setAnimation( "slideDown" );
-            setDisplayShop( "" );
+    const shopData = [
+        {   
+            link: "./productDescription",
+            image: "images/product/individual/product1.jpg",
+            title: "Knigi Issue Four",
+            discountedRegularPrice: "$18 USD",
+            discountedPrice: "$9 USD",
+            regularPrice: ""
+        },
+
+        {
+            link: "./productDescription",
+            image: "images/product/individual/product2.jpg",
+            title: "Earl of East London",
+            discountedRegularPrice: "",
+            discountedPrice: "",
+            regularPrice: "$24 USD"
+        },
+
+        {
+            link: "./productDescription",
+            image: "images/product/individual/product3.jpg",
+            title: "Earl of East London",
+            discountedRegularPrice: "",
+            discountedPrice: "",
+            regularPrice: "$14 USD"
+        },
+
+        {
+            link: "./productDescription",
+            image: "images/product/individual/product3.jpg",
+            title: "Earl of East London",
+            discountedRegularPrice: "$24 USD",
+            discountedPrice: "$12 USD",
+            regularPrice: ""
+        },
+    ]
+
+
+
+    useEffect(() => {
+        if (shopClick === 1) {
+            setAnimation("slideDown");
+            setDisplayShop("");
             setTime(Date.now());
         }
-    }, [ shopClick ]);
+    }, [shopClick]);
+
+
 
     return (
         <>
             {/* <!-- SHOP NAVIGATION MENU --> */}
             {/* <!-- START --> */}
-            <div className = { displayShop + " book-shop-separate " + animation } >
+            <div className={displayShop + " book-shop-separate " + animation} >
 
-                <div className = "bs-container flex flex-wrap flex-s-a">
-                    <a href="./productDescription.php" className = "b-s-item">
-                        <div className = "b-s-image-container">
-                            <img className = "" src="./app/img/product/individual/product1.jpg" alt=""/>
-                                <div>
-                                    <p>Woven Issue Four</p>
-                                    <p><span className = "discounted-regular-price">$18 USD</span><span className = "discounted-price"> $9 USD</span> <span className = "regular-price"></span></p>
-                                </div>
-                        </div>
+                <div className="bs-container flex flex-wrap flex-s-a">
+                    {
+                        shopData.map( ( cur ) => {
+                            return (
+                                <a href = {cur.link} className="b-s-item">
 
-                    </a>
+                                    <div className="b-s-image-container">
 
-                    <a href="./productDescription.php" className = "b-s-item">
-                        <div className = "b-s-image-container">
-                            <img className = "" src="./app/img/product/individual/product2.jpg" alt=""/>
-                                <div>
-                                    <p>Earl of East London</p>
-                                    <p><span className = "discounted-regular-price"></span><span className = "discounted-price"></span> <span className = "regular-price"> $24 USD</span></p>
-                                </div>
-                        </div>
+                                        <img className="" src={cur.image} alt="" />
 
-                    </a>
+                                        <div>
+                                            <p>{cur.title}</p>
+                                            <p>
+                                                <span className="discounted-regular-price">{cur.discountedRegularPrice}</span>
+                                                <span className="discounted-price"> {cur.discountedPrice}</span> 
+                                                <span className="regular-price"> {cur.regularPrice}</span>
+                                            </p>
+                                        </div>
 
-                    <a href="./productDescription.php" className = "b-s-item">
-                        <div className = "b-s-image-container">
-                            <img className = "" src="./app/img/product/individual/product3.jpg" alt=""/>
-                                <div>
-                                    <p>Earl of East London</p>
-                                    <p><span className = "discounted-regular-price"></span><span className = "discounted-price"></span> <span className = "regular-price"> $14 USD</span></p>
-                                </div>
-                        </div>
+                                    </div>
+                                </a>
 
-                    </a>
-
-                    <a href="./productDescription.php" className = "b-s-item">
-                        <div className = "b-s-image-container">
-                            <img className = "" src="./app/img/product/individual/product4.jpg" alt=""/>
-                                <div>
-                                    <p>Earl of East London</p>
-                                    <p><span className = "discounted-regular-price">$24 USD</span><span className = "discounted-price">$12 USD</span> <span className = "regular-price"> </span></p>
-                                </div>
-                        </div>
-
-                    </a>
+                            )
+                        })
+                    }
 
                 </div>
-                <a className = "shop-btn-shop-all-link transition" href="./shopall.php">SHOP ALL <i style={{paddingLeft : "20px;"}} className = "fas fa-chevron-right"></i></a>
+
+                <a className="shop-btn-shop-all-link transition" href="./shopall.php">SHOP ALL <i style={{ paddingLeft: "20px;" }} className="fas fa-chevron-right"></i></a>
 
 
+                <div
+                    className="empty-space-close"
+                    onMouseOver={
+                        () => {
+                            //preventing auto close
+                            let Now = Date.now();
+                            let diff = Now - time;
 
-                <div 
-                    className = "empty-space-close" 
-                    onMouseOver = {
-                            ( ) => { 
-                                //preventing auto close
-                                let Now = Date.now();
-                                let diff = Now - time;
+                            if (diff > 1000) { //if hovering is not more than one second than do not close 
+                                setTimeout(() => {
+                                    setDisplayShop("d-none");
+                                }, 370);
 
-                                if( diff > 1000 ) { //if hovering is not more than one second than do not close 
-                                    setTimeout( ( ) => {
-                                        setDisplayShop ( "d-none" ); 
-                                    }, 370 );
-                                    
-                                    setAnimation("slideUp"); 
-                                }
-
-                                
+                                setAnimation("slideUp");
                             }
-                        } 
-                >
-                
 
-                </div>
+                        }
+                    }
+                ></div>
+                
             </div>
-            {/* <!-- SHOP NAVIGATION MENU END --> */}
+
         </>
     )
 }
