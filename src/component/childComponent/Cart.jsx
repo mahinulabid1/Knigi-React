4a0d846746e27669af6aa0fb632168c05d39bfb6
Mahinul <mahinulmostafa@gmail.com>
2023-10-12 09:36:08 +0600

    added CSS module to cart.jsx

--- src/component/childComponent/Cart.jsx
+++ src/component/childComponent/Cart.jsx
@@ -1,4 +1,11 @@
 import {useState, useEffect} from "react";
+import styles from "../../../public/css/cssComponent/cart.module.css";
+
+const dataInCart = [
+    { title : "Kingi Issue 4" , price: "$ 20 USD" },
+    { title : "As You Like It ", price : "$ 15 USD"}
+]
+
 
 const Cart = ( { cartClick } ) => {
     const [ displayCart, setDisplayCart ] = useState("d-none");
@@ -13,31 +20,26 @@ const Cart = ( { cartClick } ) => {
         <>
             {/*  CART HOLDER  */}
             {/*<!-- d-none className will be here -->*/}
-            <div className= {"cart-holder " + displayCart } > 
+            <div className= { styles.cart_holder + " " + displayCart } > 
                 <div className="flex flex-s-b search-box">
-                    <div>Your Cart <span>$35 USD</span></div>
+                    <div className={styles.cartOverview}>Your Cart <span>$35 USD</span></div>
                     <div id="cart-close" onClick={()=>{setDisplayCart("d-none")}}><img src="images/icons/close.png" alt="" height="20" width="20" /></div>
                 </div>
 
-                <div className="items-in-cart-container">
-                    {/* <!-- first static item in the cart --> */}
-                    <div>
-                        <div>Woven Issue 4</div>
-                        <div>$ 20 USD</div>
-                        <div className="flex flex-align-center">
-                            <img src="images/icons/delete.png" alt="delete from the queue" height="20" width="20" />
-                        </div>
-                    </div>
-
-                    {/* <!-- second static item in the cart --> */}
-                    <div>
-                        <div>As you like it</div>
-                        <div>$ 15 USD</div>
-                        <div className="flex flex-align-center">
-                            <img src="images/icons/delete.png" alt="delete from the queue" height="20" width="20" />
-                        </div>
-                    </div>
-
+                <div>
+                    {/* cart items */}
+                    { dataInCart.map( (cur, index ) => {
+                        return(
+                            <div className={styles.cartItem + " flex flex-align-center flex-s-b"}>
+                                <div>{cur.title}</div>
+                                <div>{cur.price}</div>
+                                <div className="flex flex-align-center">
+                                    <img src="images/icons/delete.png" alt="delete from the queue" height="20" width="20" />
+                                </div>
+                            </div>
+                        )
+                    }) }
+                    
                 </div>
 
                 <div className="proceed-to-checkout"><a href="#">CHECKOUT</a></div>
