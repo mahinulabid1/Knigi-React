45a870ffce2882d529b1079740d1948da85815ef
Mahinul <mahinulmostafa@gmail.com>
2023-10-12 13:05:46 +0600

    code refactored

--- src/component/childComponent/BlogPostSummary.jsx
+++ src/component/childComponent/BlogPostSummary.jsx
@@ -1,49 +1,53 @@
+import styles from "../../../public/css/cssComponent/blogPostSummary.module.css";
+
+const BlogPostSummaryData = [
+    { 
+        images: "images/stories/stories1.jpg",
+        heading : "Making Art in the Middle of Nowhere", 
+        para: " Exploring a West Texas must-see, firmly planted at the crossroads of Art and Myth."
+    },
+
+    { 
+        images: "images/stories/stories2.jpg",
+        heading : "Closer to Home", 
+        para: "Bryce Ehrecke & Kelly Brown share how natural building could empower both a more beautiful and a more sustainable future."
+    },
+
+    { 
+        images: "images/stories/stories3.jpg",
+        heading : "Faculty Department", 
+        para: "Photographer Justin Chung’s project Faculty Department is an ode to the inspiration found in his subjects."
+    },
+
+    { 
+        images: "images/stories/stories4.jpg",
+        heading : "Antelope Canyon", 
+        para: "Worn by wind and sand, these centuries old slot canyons are a sculpted masterpiece, such that only nature could reveal."
+    }
+]
+
 const BlogPostSummary = () => {
     return (
         <>
-            <section className="blog-posts">
+            <section className={styles.blogPost}>
                 <h3>FEATURED BLOG POSTS</h3>
                 <div className="flex flex-wrap flex-cen">
-                    <div className="blog-posts-item">
-                        <div><img src="images/stories/stories4.jpg" alt=""/></div>
+                    {
+                        BlogPostSummaryData.map( ( cur ) => {
+                            return(
+                                <div className="blog-posts-item">
+                                    <div><img src={cur.images} alt=""/></div>
 
-                        <div>
-                            <h4>Making Art in the Middle of Nowhere</h4>
-                            <p>Exploring a West Texas must-see, firmly
-                                planted at the crossroads of Art and Myth.
-                            </p>
-                        </div>
-                    </div>
+                                    <div>
+                                        <h4> {cur.heading} </h4>
+                                        <p> {cur.para} </p>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
 
-                    <div className="blog-posts-item">
-                        <div><img src="images/stories/stories2.jpg" alt=""/></div>
-                        <div>
-                            <h4>Making Art in the Middle of Nowhere</h4>
-                            <p>Exploring a West Texas must-see, firmly
-                                planted at the crossroads of Art and Myth.
-                            </p>
-                        </div>
-                    </div>
-
-                    <div className="blog-posts-item">
-                        <div><img src="images/stories/stories6.jpg" alt=""/></div>
-                        <div>
-                            <h4>Making Art in the Middle of Nowhere</h4>
-                            <p>Exploring a West Texas must-see, firmly
-                                planted at the crossroads of Art and Myth.
-                            </p>
-                        </div>
-                    </div>
-
-                    <div className="blog-posts-item">
-                        <div><img src="images/stories/stories5.jpg" alt=""/></div>
-                        <div>
-                            <h4>Making Art in the Middle of Nowhere</h4>
-                            <p>Exploring a West Texas must-see, firmly
-                                planted at the crossroads of Art and Myth.
-                            </p>
-                        </div>
-                    </div>
+                    
 
                 </div>
                 <div className="visit-blog-btn"><a href="./allBlogs.php">Visit blog</a></div>
