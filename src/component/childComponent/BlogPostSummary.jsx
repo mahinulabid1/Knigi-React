b084a66b1725d5bfab30ae41646a9efc7b0d0218
Mahinul <mahinulmostafa@gmail.com>
2023-10-12 13:17:34 +0600

    code refactored

--- src/component/childComponent/BlogPostSummary.jsx
+++ src/component/childComponent/BlogPostSummary.jsx
@@ -1,30 +1,5 @@
 import styles from "../../../public/css/cssComponent/blogPostSummary.module.css";
-
-const BlogPostSummaryData = [
-    { 
-        images: "images/stories/stories1.jpg",
-        heading : "Making Art in the Middle of Nowhere", 
-        para: " Exploring a West Texas must-see, firmly planted at the crossroads of Art and Myth."
-    },
-
-    { 
-        images: "images/stories/stories2.jpg",
-        heading : "Closer to Home", 
-        para: "Bryce Ehrecke & Kelly Brown share how natural building could empower both a more beautiful and a more sustainable future."
-    },
-
-    { 
-        images: "images/stories/stories3.jpg",
-        heading : "Faculty Department", 
-        para: "Photographer Justin Chung’s project Faculty Department is an ode to the inspiration found in his subjects."
-    },
-
-    { 
-        images: "images/stories/stories4.jpg",
-        heading : "Antelope Canyon", 
-        para: "Worn by wind and sand, these centuries old slot canyons are a sculpted masterpiece, such that only nature could reveal."
-    }
-]
+import { BlogPostSummaryData } from "./dataAndFunctions";
 
 const BlogPostSummary = () => {
     return (
