f53c61d12dd4d12d0ff27b2ae7adecbeec42777c
Mahinul <mahinulmostafa@gmail.com>
2023-10-19 11:07:42 +0600

    code refactored

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,7 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-
-import Homepage from "./component/index"
-import StoriesPage from "./component/storiesDetailedPage"
-import ShopAll from './component/shopAll';
-import AllStoriesPage from './component/allStoriesPage';
-import AllBlogs from './component/allBlog';
-import ContactUs from './component/contactus';
-import TermsAndCondition from './component/termsAndCondition';
-import RequestInterView from './component/requestInterview';
+import routeHandler from './routeHandler';
 
 // universal css files
 // I can access this rules in every Component
@@ -20,53 +12,9 @@ import "../public/css/cssComponent/universal_css.css"
 
 
 
-const router = createBrowserRouter ( [
-  {
-    path: "/",
-    element: <Homepage/>,
-  },
-  
-  {
-    path: "/allStories",
-    element : <AllStoriesPage />
-  },
-
-  {
-    path: "/shopall",
-    element: <ShopAll />
-  },
-
-  {
-    path: "/storiesDetail",
-    element: <StoriesPage />
-  },
-
-  {
-    path: "/blog",
-    element: <AllBlogs />
-  },
-  
-  {
-    path: "/contactus",
-    element: <ContactUs />
-  },
-
-  {
-    path: "/termsandcondition",
-    element: <TermsAndCondition />
-  },
-
-  {
-    path: "/requestinterview",
-    element : <RequestInterView/>
-  },
-
-  
-] );
-
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={routeHandler} />
   </React.StrictMode>
 );
