ce5e1418a682da5fb568be31019e4328a139c51c
Mahinul <mahinulmostafa@gmail.com>
2023-11-21 00:12:51 +0600

    Completed ShopAll page with database interaction

--- src/component/shopAll.jsx
+++ src/component/shopAll.jsx
@@ -1,8 +1,101 @@
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 import styles from "../../public/css/shopAll.module.css"
 import Navigation from './childComponent/nav'
+import Footer from './childComponent/footer'
+import BlogPostSummary from './childComponent/BlogPostSummary'
+
+
+
+const RegularPrice = ( data ) => {
+    console.log(data);
+    return (
+        <>
+            <span className = { styles.regular_price }> {"$" + data.regularPrice + " USD"} </span>
+        </>
+    )
+}
+
+
+
+const PriceWithDisount = ( {dataOne, dataTwo} ) => {
+
+    return (
+        <>
+            <span className = { styles.regular_price_before_discount } > { "$" + dataOne + " USD" } </span>
+            <span className = { styles.discounted_price } > { "$" + dataTwo + " USD" } </span> 
+        </>
+    )
+}
+
+
+
+const ShopItem = ( {data} ) => {
+    /*
+    Important Message for learning purpose
+    =======================================
+    -why did I create this seperate component?
+    1. Sending request using AXIOS is asyncronous.
+    2. Getting data from API takes time.
+    3. If the component gets rendered before API-data-fetching completes,
+       then it will thorw error since I'm trying to render a data that's undefined
+    4. This component will only render if API-data-fetching process completes.
+    */
+
+
+    return (
+        <>
+
+            { data.map( ( cur,index ) => {
+
+                return(
+                    <a href = { "/shopItemDetails/" + cur._id } className = { styles.product_link } key = { index.toString() } >
+
+                        < div className = { styles.image_grey_box } >
+                            <img className = "center-middle" src = { cur.productImage } alt = "" />
+                        </div>
+
+                        <div className = { styles.product_text }>
+                            <p className = { styles.product_name }> { cur.productTitle } </p>
+
+                            <p className = { styles.product_price }>
+                                { 
+                                    cur.productPrice.discountedPrice === undefined ? 
+                                    <RegularPrice data={cur.productPrice.regularPrice} /> 
+                                    : 
+                                    <PriceWithDisount dataOne = {cur.productPrice.regularPriceBeforeDiscount} dataTwo = { cur.productPrice.discountedPrice } />
+                                }
+                            </p>
+                        </div>
+                    </a>
+                )
+
+            }) } {/* Ending of { } */}
+
+
+        </>
+    ) 
+}
+
+
+
+
+
 
 
 const ShopAll = ( ) => {
+
+    const [data, setData] = useState(undefined);
+    useEffect( ( ) => {
+        axios.get('http://localhost:8000/api/v1/shoplist?limit=20')
+        .then((result) => {
+            // result.data contains fetched information from Database
+            console.log(result.data)
+            setData(result.data);
+           
+        })
+    }, [])
+
     return (
         <>  
             <Navigation />
@@ -15,138 +108,25 @@ const ShopAll = ( ) => {
                 <div className = {styles.heroTextCorner} >SHIPS WORLDWIDE</div>
             </header>
 
-            <section className = {styles.booksContainer}>
+            <section className = {styles.booksSection}>
 
                 <div className = {styles.categoryNavigation + " flex flex-s-b"}>
                     <div className = "books-nav-item">WOVEN GOODS</div>
                     <div className = "books-nav-item">
                         <span className = "individual-btn category-activated">INDIVIDUAL</span> 
-                        / 
-                        <span className = "wholesale-btn">WHOLESALE</span></div>
+                    </div>
                 </div>
 
-                <div className = "books-item-flex-container individual">
+                <div className ={"flex flex-wrap flex-s-b bound-navigation " + styles.listContainer} >
 
-                    <a href="./productDescription.php" className = "product-item">
-                        <div className = "product-image-container">
-                            <img className = "center-middle" src="./app/img/product/individual/product1.jpg" alt=""/>
-                        </div>
-                        <div className = "product-text">
-                            <p className = "product-name">Woven Issue Four – Seconds Sale</p>
-                            <p className = "product-price"><span className = "discounted-regular-price">$18 USD</span><span className = "discounted-price"> $9 USD</span> <span className = "regular-price"></span></p>
-                        </div>
-                    </a>
+                    { data !== undefined ? <ShopItem data={data}/> : console.log("Data hasn't arrived yet") }
 
-                    <a href="./productDescription.php" className = "product-item">
-                        <div className = "product-image-container">
-                            <img className = "center-middle" src="./app/img/product/individual/product2.jpg" alt=""/>
-                        </div>
-                        <div className = "product-text">
-                            <p className = "product-name">Earl of East London – Strand</p>
-                            <p className = "product-price"><span className = "discounted-regular-price"></span><span className = "discounted-price"></span> <span className = "regular-price"> $24 USD</span></p>
-                        </div>
-                    </a>
-
-                    <a href="./productDescription.php" className = "product-item">
-                        <div className = "product-image-container">
-                            <img className = "center-middle" src="./app/img/product/individual/product3.jpg" alt=""/>
-                        </div>
-                        <div className = "product-text">
-                            <p className = "product-name">Earl of East London – Strand</p>
-                            <p className = "product-price"><span className = "discounted-regular-price"></span><span className = "discounted-price"></span> <span className = "regular-price"> $14 USD</span></p>
-                        </div>
-                    </a>
-
-                    <a href="./productDescription.php" className = "product-item">
-                        <div className = "product-image-container">
-                            <img className = "center-middle" src="./app/img/product/individual/product4.jpg" alt=""/>
-                        </div>
-                        <div className = "product-text">
-                            <p className = "product-name">Earl of East London – Strand</p>
-                            <p className = "product-price"><span className = "discounted-regular-price">$24 USD</span><span className = "discounted-price">$12 USD</span> <span className = "regular-price"> </span></p>
-                        </div>
-                    </a>
-
-                    <a href="./productDescription.php" className = "product-item">
-                        <div className = "product-image-container">
-                            <img className = "center-middle" src="./app/img/product/individual/product5.jpg" alt=""/>
-                        </div>
-                        <div className = "product-text">
-                            <p className = "product-name">Earl of East London – Strand</p>
-                            <p className = "product-price"><span className = "discounted-regular-price"></span> <span className = "discounted-price"></span><span className = "regular-price"> $44 USD</span></p>
-                        </div>
-                    </a>
-                    
-                    <a href="./productDescription.php" className = "product-item">
-                        <div className = "product-image-container">
-                            <img className = "center-middle" src="./app/img/product/individual/product6.jpg" alt=""/>
-                        </div>
-                        <div className = "product-text">
-                            <p className = "product-name">Earl of East London – Strand</p>
-                            <p className = "product-price"><span className = "discounted-regular-price"></span><span className = "discounted-price"></span> <span className = "regular-price"> $22 USD</span></p>
-                        </div>
-                    </a>
-                </div>
-
-
-                <div className = "books-item-flex-container wholesale">
-                    <a href="./productDescription.php" className = "product-item">
-                        <div className = "product-image-container">
-                            <img className = "center-middle" src="./app/img/product/wholesale/stack1.jpg" alt=""/>
-                        </div>
-                        <div className = "product-text">
-                            <p className = "product-name">Earl of East London – Strand</p>
-                            <p className = "product-price"><span className = "discounted-regular-price"></span><span className = "discounted-price"></span> <span className = "regular-price"> $24 USD</span></p>
-                        </div>
-                    </a>
-                    <a href="./productDescription.php" className = "product-item">
-                        <div className = "product-image-container">
-                            <img className = "center-middle" src="./app/img/product/wholesale/stack2.jpg" alt=""/>
-                        </div>
-                        <div className = "product-text">
-                            <p className = "product-name">Earl of East London – Strand</p>
-                            <p className = "product-price"><span className = "discounted-regular-price"></span><span className = "discounted-price"></span> <span className = "regular-price"> $24 USD</span></p>
-                        </div>
-                    </a>
-                    <a href="./productDescription.php" className = "product-item">
-                        <div className = "product-image-container">
-                            <img className = "center-middle" src="./app/img/product/wholesale/stack3.jpg" alt=""/>
-                        </div>
-                        <div className = "product-text">
-                            <p className = "product-name">Earl of East London – Strand</p>
-                            <p className = "product-price"><span className = "discounted-regular-price"></span><span className = "discounted-price"></span> <span className = "regular-price"> $24 USD</span></p>
-                        </div>
-                    </a>
-                    <a href="./productDescription.php" className = "product-item">
-                        <div className = "product-image-container">
-                            <img className = "center-middle" src="./app/img/product/wholesale/stack4.jpg" alt=""/>
-                        </div>
-                        <div className = "product-text">
-                            <p className = "product-name">Earl of East London – Strand</p>
-                            <p className = "product-price"><span className = "discounted-regular-price"></span><span className = "discounted-price"></span> <span className = "regular-price"> $24 USD</span></p>
-                        </div>
-                    </a>
-                    <a href="./productDescription.php" className = "product-item">
-                        <div className = "product-image-container">
-                            <img className = "center-middle" src="./app/img/product/wholesale/stack5.jpg" alt=""/>
-                        </div>
-                        <div className = "product-text">
-                            <p className = "product-name">Earl of East London – Strand</p>
-                            <p className = "product-price"><span className = "discounted-regular-price"></span><span className = "discounted-price"></span> <span className = "regular-price"> $24 USD</span></p>
-                        </div>
-                    </a>
-                    <a href="./productDescription.php" className = "product-item">
-                        <div className = "product-image-container">
-                            <img className = "center-middle" src="./app/img/product/wholesale/stack6.jpg" alt=""/>
-                        </div>
-                        <div className = "product-text">
-                            <p className = "product-name">Earl of East London – Strand</p>
-                            <p className = "product-price"><span className = "discounted-regular-price"></span><span className = "discounted-price"></span> <span className = "regular-price"> $24 USD</span></p>
-                        </div>
-                    </a>
                 </div>
 
             </section>
+
+            <BlogPostSummary />
+            <Footer />
         </>
     )
 }
